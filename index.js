f80e7d8481d0d831ae3ee7614381597d7bb3513e
Kevin McNeil <dogwoodrawks@gmail.com>
2020-10-14 18:18:08 -0700

    lab03

--- index.js
+++ index.js
@@ -6,14 +6,25 @@ const app = express();
 const input = require('./lib/input.js');
 const note = require('./lib/notes.js');
 let argv = process.argv;
+const mongoose = require('mongoose');
+
 
 let newNote = new input(argv);
-let obj =  new note(newNote);
+let obj = new note(newNote);
 console.log(obj);
+obj.execute();
+
+mongoose.connect('mongodb://localhost:27017/notes', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => console.log('we are connected to mongo'));
+
+
 
 app.get('/status', (request, response) => {
   const currentStatus = status(request);
   response.status(200).json(currentStatus);
 });
 
-app.listen(process.env.PORT, () => console.log('server up') );
+app.listen(process.env.PORT, () => console.log('server up'));
